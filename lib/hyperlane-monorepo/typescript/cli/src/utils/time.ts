f5523a8a3ee5d068722c040db68f753659a20565
Mark Ballew (1898145) <ballew@berkeley.edu>
2025-11-22 15:52:35 -0800

    Remove stale files

--- lib/hyperlane-monorepo/typescript/cli/src/utils/time.ts
+++
@@ -1,6 +0,0 @@
-export function getTimestampForFilename() {
-  const now = new Date();
-  return `${now.getFullYear()}-${
-    now.getMonth() + 1
-  }-${now.getDate()}-${now.getHours()}-${now.getMinutes()}-${now.getSeconds()}`;
-}
